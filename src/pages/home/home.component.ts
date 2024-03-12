9ce20b7c1e63a32e1bf6a8a04c4f8e67dd74e92b
Yogi <85734470+16Yogi@users.noreply.github.com>
2024-03-12 23:28:25 +0530

    commit

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { HedderComponent } from '../../component/hedder/hedder.component';
 import { BannerComponent } from '../../component/banner/banner.component';
+import { AboutcpComponent } from '../../component/aboutcp/aboutcp.component';
+import { NewsComponent } from '../../component/news/news.component';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
 
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,RouterOutlet,HedderComponent,BannerComponent],
+  imports: [CommonModule,RouterOutlet,HedderComponent,BannerComponent,AboutcpComponent,NewsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

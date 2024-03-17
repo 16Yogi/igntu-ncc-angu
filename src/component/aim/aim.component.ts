7f1d6d277a3b7253fb4f14e0cf44a062e7c34af7
Yogi <85734470+16Yogi@users.noreply.github.com>
2024-03-17 23:59:04 +0530

    commit

---
+++ src/component/aim/aim.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aim',
+  standalone: true,
+  imports: [],
+  templateUrl: './aim.component.html',
+  styleUrl: './aim.component.css'
+})
+export class AimComponent {
+
+}
--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -3,6 +3,9 @@ import { HedderComponent } from '../../component/hedder/hedder.component';
 import { BannerComponent } from '../../component/banner/banner.component';
 import { AboutcpComponent } from '../../component/aboutcp/aboutcp.component';
 import { NewsComponent } from '../../component/news/news.component';
+import { FooterComponent } from '../../component/footer/footer.component';
+import { AimComponent } from '../../component/aim/aim.component';
+import { ContactComponent } from '../../component/contact/contact.component';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -12,7 +15,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,RouterOutlet,HedderComponent,BannerComponent,AboutcpComponent,NewsComponent],
+  imports: [CommonModule,RouterOutlet,HedderComponent,BannerComponent,AboutcpComponent,NewsComponent,AimComponent,ContactComponent,FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
